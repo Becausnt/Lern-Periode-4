063691709e252056bd1240f0b2be1dc616f404b5
Becausnt <142886938+Becausnt@users.noreply.github.com>
2024-03-05 11:03:16 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using GameBot.modules;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GameBot
+{
+    internal class Program
+    {
+        static void Main(string[] args) => new Program().RunBotAsync().GetAwaiter().GetResult();
+
+
+        DiscordSocketClient _client; // das Problem war, das dies als private definiert wurde.
+        private CommandService _commands;
+        private IServiceProvider _services;
+
+        public async Task RunBotAsync()
+        {
+
+            var config = new DiscordSocketConfig()
+            {
+                // Other config options can be presented here.
+                GatewayIntents = GatewayIntents.AllUnprivileged | GatewayIntents.MessageContent
+            };
+
+            _client = new DiscordSocketClient(config);
+            _commands = new CommandService();
+            _services = new ServiceCollection()
+                .AddSingleton(_client)
+                .AddSingleton(_commands)
+                .BuildServiceProvider() ;
+
+
+            var cmd = new commands();
+
+            string token = "MTIwOTQwMDIxOTE0MjMyNDIyNA.GpK6iV.RnrkVK9J7GK1V1Ax0jf_JSXIBuYI-9pWdnxNYU";
+            _client.Log += _client_Log;
+            _client.ButtonExecuted += MyButtonHandler;
+            _client.SelectMenuExecuted += cmd.MenuHandler;
+
+            await RegisterCommandAsync();
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+            await Task.Delay(-1);
+        }
+
+        private Task _client_Log(LogMessage arg)
+        {
+            Console.WriteLine(arg);
+            return Task.CompletedTask;
+        }
+
+        public async Task RegisterCommandAsync()
+        {
+            _client.MessageReceived += HandleCommandAsync;
+            await _commands.AddModuleAsync(typeof(commands), _services);
+        }
+
+        private async Task HandleCommandAsync(SocketMessage arg)
+        {
+            var message = arg as SocketUserMessage;
+            
+            var context = new SocketCommandContext(_client, message);
+            if (message.Author.IsBot) return;
+
+            int argPos = 0;
+            if (message.HasCharPrefix('/', ref argPos))
+            {
+                var result = await _commands.ExecuteAsync(context, argPos, _services);
+                if (!result.IsSuccess) Console.WriteLine(result.ErrorReason);
+            }
+        }
+        public async Task MyButtonHandler(SocketMessageComponent component)
+        {
+            // We can now check for our custom id
+            switch (component.Data.CustomId)
+            {
+                // Since we set our buttons custom id as 'custom-id', we can check for it like this:
+                case "test-btn":
+                    // Lets respond by sending a message saying they clicked the button
+                    await component.RespondAsync($"{component.User.Mention} has clicked the button!");
+                    break;
+            }
+        }
+
+
+    }
+}
